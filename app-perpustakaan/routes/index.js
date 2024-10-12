04c8fee8169e48307d8fd54dc742b4afcc606abe
ericcandra <ericcandra_2226240059@mhs.mdp.ac.id>
2024-10-12 22:07:21 +0700

    home
    saya membuat halaman home

--- app-perpustakaan/routes/index.js
+++ app-perpustakaan/routes/index.js
@@ -3,8 +3,24 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+  //   res.render('home', { title: 'Express', layout:'main' });
+  const buku =[
+      { 
+          buku: "Si Kancil",
+          cerita: "Tentang si kancil pintar dan baijak"  
+          
+      },
+      {
+          buku: "Anak Durhaka",
+          cerita: "Maling kundang si anak durhaka"
+      },
+      {
+          buku: "Putri Duyung",
+          cerita: "Putri duyung yang menyasar ke daratan"
+      }
+  ];
+  res.render('home',{title: 'halaman home',buku, layout:'main'});
+  });
 
 router.get('/about' , function(req, res, next) {
   res.render('about',{title: 'halaman about', layout:'main'});
