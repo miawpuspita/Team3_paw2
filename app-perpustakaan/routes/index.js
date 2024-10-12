e1f5faffd131f5acbbb6cc81ceeb0d72b4787e9a
ericcandra <ericcandra_2226240059@mhs.mdp.ac.id>
2024-10-12 22:27:14 +0700

    buku dan route json
    saya membuat halaman buku dan route json

--- app-perpustakaan/routes/index.js
+++ app-perpustakaan/routes/index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+
+
 router.get('/', function(req, res, next) {
   //   res.render('home', { title: 'Express', layout:'main' });
   const buku =[
@@ -25,10 +27,48 @@ router.get('/', function(req, res, next) {
 router.get('/about' , function(req, res, next) {
   res.render('about',{title: 'halaman about', layout:'main'});
 });
+
 router.get('/contact', function(req, res, next) {
   res.render('contact', { title: 'halaman contact', layout: 'main' });
 });
 
+router.get('/buku' , function(req, res, next) {
+  const daftarbuku =[
+    {NamaBuku: "Si Kancil", Penulis: "Eric", Tahun: "2006"},
+    {NamaBuku: "Maling Kundang", Penulis: "Nabila", Tahun: "2009"},
+    {NamaBuku: "Anak Pintar", Penulis: "Puspita", Tahun: "2010"},
+    {NamaBuku: "Bawang merah", Penulis: "Angel", Tahun: "2011"},
+    {NamaBuku: "Putri Duyung", Penulis: "Darren", Tahun: "2012"},
+    {NamaBuku: "Si Kembar", Penulis: "Critiano", Tahun: "2015"}
+];
+  res.render('buku',{title: 'halaman buku',daftarbuku, layout:'main'});
+});
 
+router.get("/bukus",(req, res)=>{
+  res.json({
+      "status" : "Success",
+      "message" : "Data Buku",
+      "data" : [{namaBuku: "si kancil", penulis: "Eric Candra"},{namaBuku: "maling kundang", penulis: "Nabila"}]
+  })
+});
 
+router.get("/pinjam",(req, res)=>{
+  res.json({
+      "status" : "Success",
+      "message" : "Data Pinjam",
+      "data" : [{nama: "eric candra", tanggal: "14 oktober 2024"},{nama: "jovan", tanggal: "25 oktober 2024"}]
+  })
+});
+
+router.get("/petugas",(req, res)=>{
+  res.json({
+      "status" : "Success",
+      "message" : "Data Petugas",
+      "data" : [{namaPetugas: "Dareen", tugas: "security"},{namaPetugas: "Cristiano", tugas: "admin"}]
+  })
+});
+
+router.use("/",(req, res)=>{
+  res.send("<h1>404 not found</h1>");
+});
 module.exports = router;
